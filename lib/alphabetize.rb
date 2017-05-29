1c30fddb706098fc15bfb447eff3f42c457d60a2
Jexeones24 <jexeones@gmail.com>
2017-05-29 12:12:21 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,12 +2,11 @@ require 'pry'
 def alphabetize(arr)
   e_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
   arr.sort_by do |str|
-    str.chars.map { |l| e_alpha.index(l) } #what is this doing? can't figure it out
-    
+    str.chars.map { |l| e_alpha.index(l) } 
   end
 end
 
-#sort_by enumerates through each string
-#sorts each string by word
-#converting strings to chars and mapping each
-#comparing chars of e_alpha to letters of string
+#sort_by enumerates through each string in array
+#enumerating thru each string by word, converting strings to chars
+#map invokes block for each letter, creates new array
+#comparing chars of e_alpha to letters of string and sorting by result
